901b44a6832595dd06dd9fad53226cbdb4d45779
leasunhy <leasunhy@gmail.com>
2015-12-21 15:22:58 +0800

    add Contest model

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,7 +1,8 @@
 from .user import User
 from .problem import Problem, ProblemTag
 from .submission import Submission
+from .contest import Contest
 
-model_list = [User, Problem, Submission, ProblemTag]
+model_list = [User, Problem, Submission, ProblemTag, Contest]
 model_dict = dict(map(lambda x: (x.__name__, x), model_list))
 
---
+++ server/models/contest.py
@@ -0,0 +1,43 @@
+from werkzeug import generate_password_hash, check_password_hash
+
+from .problem import Problem
+from .. import db
+
+contest_problem_rel = db.Table('contest_problem_rel',
+        db.Column('contest_id', db.Integer, db.ForeignKey('contest.id')),
+        db.Column('problem_id', db.Integer, db.ForeignKey('problem.id'))
+)
+
+class Contest(db.Model):
+    """Contest model."""
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.Text)
+
+    start_time = db.Column(db.DateTime, nullable=False)
+    end_time = db.Column(db.DateTime, nullable=False)
+
+    passcode_hash = db.Column(db.String(128))
+
+    @property
+    def passcode(self):
+        return "What are you doing?"
+
+    @passcode.setter
+    def passcode(self, value):
+        self.passcode_hash = generate_password_hash(value)
+
+    def verify_password(self, password):
+        return self.passcode_hash is None or\
+               check_password_hash(self.passcode_hash, password)
+
+    # relationships
+    problems = db.relationship('Problem', secondary='contest_problem_rel',
+                    backref=db.backref('contests', lazy='dynamic'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return '<Contest %s>' % self.title
+
