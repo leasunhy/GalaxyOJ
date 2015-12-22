ae41528f27be8e45b79fd2271d449f7c4658af16
leasunhy <leasunhy@gmail.com>
2015-12-22 13:12:20 +0800

    refactor judge directory

---
+++ judge/__main__.py
@@ -0,0 +1,4 @@
+from .worker import run
+
+run()
+
--- judge/worker.py
+++ judge/worker.py
@@ -9,7 +9,7 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 conn = redis.from_url(redis_url)
 
-if __name__ == '__main__':
+def run():
     with Connection(conn):
         worker = Worker(list(map(Queue, listen)))
         worker.work()
