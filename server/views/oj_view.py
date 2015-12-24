b3c14ac3f608424d85ad5f79d45c243baf3bb6ed
mstczuo <mstczuo@163.com>
2015-12-24 18:55:31 +0800

    Add submit code view module

--- server/views/oj_view.py
+++ server/views/oj_view.py
@@ -1,6 +1,9 @@
 from . import oj
 
 from flask import render_template, url_for, request, redirect, flash, abort
+from flask.ext.login import login_required, current_user
+from .. import db
+from ..forms import SubmissionForm
 from ..models import Problem, Contest, Submission
 
 @oj.route('/problems')
@@ -45,3 +48,32 @@ def problem(cid = 0, pid = 1):
             abort(404)
     return render_template('show_problem.html', p=problem)
 
+def save_to_file(data, submission_id)
+    f = open(file_name, 'w')
+    f.write(data)
+    f.close()
+
+@login_required
+@oj.route('/submit/<int:pid>', methods = ['GET', 'POST'])
+@oj.route('/contest/<int:cid>/submit/<int:pid>', methods = ['GET', 'POST'])
+def submit_code(cid = 0, pid = 1):
+    if cid == 0:
+        problem = Problem.query.get_or_404(pid)
+    else:
+        contest = Contest.query.get_or_404(cid)
+        try:
+            problem = contest.problems[pid-1]
+        except IndexError:
+            abort(404)
+    form = SubmissionForm()
+    if form.validate_on_submit():
+        submit = Submission()
+        submit.owner = current_user
+        submit.problem = problem
+        submit.compiler_id = form.compiler.data
+        db.session.add(submit)
+        db.session.commit()
+        save_to_file(form.code.data, submit.id)
+        return "haha"
+    print("Invalid")
+    return render_template('submit_code.html', form = form, cid = cid, pid = pid)
