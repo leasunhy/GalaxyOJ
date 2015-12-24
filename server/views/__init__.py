220b1de891f647555ca85468fbbcebe4574443bf
leasunhy <leasunhy@gmail.com>
2015-12-24 13:44:23 +0800

    find back our judge test function :-)

--- server/views/__init__.py
+++ server/views/__init__.py
@@ -5,13 +5,16 @@ post = Blueprint('post', __name__)
 auth = Blueprint('auth', __name__)
 admin = Blueprint('admin', __name__)
 oj = Blueprint('oj', __name__)
+judge = Blueprint('judge', __name__)
 
 views = [(main, ''),
          (post, '/post'),
          (auth, '/auth'),
          (admin, '/admin'),
-         (oj, '/oj')
+         (oj, '/oj'),
+         (judge, '/judge'),
 ]
 
-from . import main_view, post_view, auth_view, admin_view, oj_view
+from . import main_view, post_view, auth_view, admin_view, oj_view, judge_view
+
 
