af0de2a1d059b7a9fb04772a99dbd1a991d5d4a4
leasunhy <leasunhy@gmail.com>
2015-12-25 15:57:37 +0800

    beautify compiler names

--- judge/config.py
+++ judge/config.py
@@ -12,7 +12,7 @@ COMPILE_TIME_LIMIT = 10
 # compiler options
 COMPILER_CNT = 4
 COMPILER_LIST = ["g++", "gcc", "fpc", "g++", "ghc"]
-COMPILER_NAME_LIST = ["g++ 5.3.0", "gcc 5.3.0", "fpc 3.0.0", "c++11 5.3.0", "Haskell GHC 7.10.3"]
+COMPILER_NAME_LIST = ["G++ 5.3.0", "GCC 5.3.0", "Free Pascal 3.0.0", "G++ C++11 5.3.0", "Haskell GHC 7.10.3"]
 COMPILER_OPTION_LIST = [
         ["-o", "a.out", "-O2", "-Wall", "-lm", "--static", "-DONLINE_JUDGE"],
         ["-o", "a.out", "-O2", "-Wall", "-lm", "--static", "-std=c99", "-DONLINE_JUDGE"],
