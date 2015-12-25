ece0ba5dd1e89222c4d05c2e2a62b4ae7c0b70e2
leasunhy <leasunhy@gmail.com>
2015-12-25 15:53:29 +0800

    add haskell :-)

--- judge/config.py
+++ judge/config.py
@@ -11,11 +11,13 @@ COMPILE_TIME_LIMIT = 10
 
 # compiler options
 COMPILER_CNT = 4
-COMPILER_LIST = ["g++", "gcc", "fpc", "g++"]
-COMPILER_NAME_LIST = ["g++ 5.3.0", "gcc 5.3.0", "fpc 3.0.0", "c++11 5.3.0"]
+COMPILER_LIST = ["g++", "gcc", "fpc", "g++", "ghc"]
+COMPILER_NAME_LIST = ["g++ 5.3.0", "gcc 5.3.0", "fpc 3.0.0", "c++11 5.3.0", "Haskell GHC 7.10.3"]
 COMPILER_OPTION_LIST = [
         ["-o", "a.out", "-O2", "-Wall", "-lm", "--static", "-DONLINE_JUDGE"],
         ["-o", "a.out", "-O2", "-Wall", "-lm", "--static", "-std=c99", "-DONLINE_JUDGE"],
         ["-oa.out", "-O1", "-Co", "-Cr", "-Ct", "-Ci"],
-        ["-o", "a.out", "-O2", "-Wall", "-lm", "-std=c++11", "--static", "-DONLINE_JUDGE"]]
-COMPILER_FILEEXT_LIST = [".cpp", ".c", ".pas", ".cpp"]
+        ["-o", "a.out", "-O2", "-Wall", "-lm", "-std=c++11", "--static", "-DONLINE_JUDGE"],
+        ["-o", "a.out", "--make", "-O"],
+]
+COMPILER_FILEEXT_LIST = [".cpp", ".c", ".pas", ".cpp", ".hs"]
