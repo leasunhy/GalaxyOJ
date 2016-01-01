abd6aa86f8b6d8ec2d0ac034df6b4dfc78eeb7ba
leasunhy <leasunhy@gmail.com>
2016-01-01 18:32:11 +0800

    add upload view

--- server/views/__init__.py
+++ server/views/__init__.py
@@ -6,6 +6,7 @@ auth = Blueprint('auth', __name__)
 admin = Blueprint('admin', __name__)
 oj = Blueprint('oj', __name__)
 judge = Blueprint('judge', __name__)
+upload = Blueprint('upload', __name__)
 
 views = [(main, ''),
          (post, '/post'),
@@ -13,8 +14,10 @@ views = [(main, ''),
          (admin, '/admin'),
          (oj, '/oj'),
          (judge, '/judge'),
+         (upload, '/upload'),
 ]
 
-from . import main_view, post_view, auth_view, admin_view, oj_view, judge_view
+from . import main_view, post_view, auth_view,\
+              admin_view, oj_view, judge_view, upload_view
 
 
---
+++ server/views/upload_view.py
@@ -0,0 +1,47 @@
+from . import upload
+
+from werkzeug.security import safe_join
+from werkzeug import secure_filename
+from flask import request, jsonify, url_for, current_app, abort, send_from_directory
+
+import hashlib
+from datetime import datetime
+
+
+# for CKEDITOR
+@upload.route('/upload_image', methods=['POST'])
+def upload_image():
+    def allowed_file(filename):
+        return '.' in filename and \
+               filename.rsplit('.', 1)[1].lower() in ['jpg', 'png']
+
+    file = request.files['image']
+    error = ''
+    if not file:
+        return jsonify(uploaded=0, error={'message': 'Please select a file.'})
+    elif not allowed_file(file.filename):
+        return jsonify(uploaded=0, error={'message': 'File extension not allowed.'})
+    else:
+        filename = file.filename
+        extension = '.' + filename.rsplit('.', 1)[1]
+        filename = hashlib.md5(filename + str(datetime.utcnow())).hexdigest() + extension
+        filepath = safe_join(current_app.config['IMAGE_UPLOAD_FOLDER'], filename)
+        if not filepath:
+            return jsonify(uploaded=0, error={'message': 'Filename illegal.'})
+        file.save(filepath)
+        callback = request.args.get('CKEditorFuncNum')
+        url = url_for('upload.fetch', filename=filename, _external=True)
+        res = """
+            <script type="text/javascript">window.parent.CKEDITOR.tools.callFunction(%s, '%s', '%s')</script>
+        """ % (callback, url, error)
+        return res, 200, {"Content-Type": "text/html"}
+
+
+@upload.route('/fetch_image/<path:filename>')
+def fetch(filename):
+    filename = secure_filename(filename.strip())
+    if not filename:
+        abort(404)
+    return send_from_directory(current_app.config['IMAGE_UPLOAD_FOLDER'], filename)
+
+
