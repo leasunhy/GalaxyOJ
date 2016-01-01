c5c541e2afaee6f1e139965435c82fd6754a6593
leasunhy <leasunhy@gmail.com>
2016-01-01 14:06:57 +0800

    add datetime format filter

--- server/tools/__init__.py
+++ server/tools/__init__.py
@@ -1,3 +1,4 @@
 from .privilege import privilege_required
 from .sanitize import sanitize
+from .datetime import datetime_format
 
---
+++ server/tools/datetime.py
@@ -0,0 +1,3 @@
+def datetime_format(value, format='%Y-%m-%d %H:%M:%S'):
+    return value.strftime(format)
+
