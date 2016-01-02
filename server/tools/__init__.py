d8eb9a7ded9bd2b16593301a687951e12f097f2f
leasunhy <leasunhy@gmail.com>
2016-01-02 18:30:07 +0800

    add count_page utility function

--- server/tools/__init__.py
+++ server/tools/__init__.py
@@ -1,5 +1,5 @@
-from .privilege import privilege_required, root_required
+from .privilege import privilege_required, root_required, ROOT_PRIVILEGE
 from .sanitize import sanitize
 from .datetime import datetime_format
+from .misc import count_page
 
-from .privilege import ROOT_PRIVILEGE
